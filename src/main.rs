77dbce24df8ef48b2cf22f6c168db82df017e328
Josh Mcguigan <joshmcg88@gmail.com>
2018-05-15 22:37:19 -0700

    Create seperate server instances to listen on GET and POST ports

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,11 @@ use rocket::data::Outcome;
 use rocket::outcome::Outcome::Failure;
 use rocket::http::Status;
 use rocket::response::Content;
+use std::sync::Arc;
+use rocket::Config;
+use rocket::config::Environment;
+use std::thread;
+use std::time;
 
 #[derive(Clone, Debug)]
 struct SubmittedData {
@@ -47,7 +52,7 @@ struct AppData {
 }
 
 #[get("/api/<path..>")]
-fn read_data(app_data: State<AppData>, path: PathBuf) -> Content<Option<Vec<u8>>> {
+fn read_data(app_data: State<Arc<AppData>>, path: PathBuf) -> Content<Option<Vec<u8>>> {
     let data = app_data.data.lock().unwrap();
     match data.get(path.to_str().unwrap()) {
         Some(submitted_data) => {
@@ -59,14 +64,30 @@ fn read_data(app_data: State<AppData>, path: PathBuf) -> Content<Option<Vec<u8>>
 }
 
 #[post("/<path..>", data="<post_data>")]
-fn write_data(app_data: State<AppData>, path: PathBuf, post_data: SubmittedData) {
+fn write_data(app_data: State<Arc<AppData>>, path: PathBuf, post_data: SubmittedData) {
     let mut data = app_data.data.lock().unwrap();
     data.insert(String::from(path.to_str().unwrap()), post_data);
 }
 
 
 fn main() {
-    rocket::ignite().mount("/", routes![write_data, read_data])
-        .manage(AppData {..Default::default()})
+    let write_app_state = Arc::new(AppData {..Default::default()});
+    let read_app_state = write_app_state.clone();
+
+    let write_config = Config::build(Environment::active().unwrap()).port(3000).unwrap();
+    let read_config = Config::build(Environment::active().unwrap()).port(8000).unwrap();
+
+    thread::spawn(   || {
+        rocket::custom(write_config, false).mount("/", routes![write_data])
+            .manage(write_app_state)
+            .launch();
+    });
+
+    rocket::custom(read_config, false).mount("/", routes![read_data])
+        .manage(read_app_state)
         .launch();
+
+    // TODO log when server starts
+    // TODO allow user to specify which ports to use
+    // TODO allow static file serving on GET server
 }
